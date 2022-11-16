51f6bc77f0633ce1a3c73eb504f9b4d31901fc0d
孟峰 <mengfeng@mengfengdeMacBook-Pro.local>
2022-11-16 09:14:28 +0800

    biu

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
  * @Author: alan_mf
  * @Date: 2022-11-15 08:31:43
  * @LastEditors: alan_mf
- * @LastEditTime: 2022-11-16 08:54:01
+ * @LastEditTime: 2022-11-16 09:14:16
  * @FilePath: /docker-test/app.js
  * @Description: 
  * 
  */
 let res = function(){
-    console.log("hello word")
+    console.log("hello word 1221")
 }
 res()
